4bfcde6e290741d1cddecbd5c0bc24a782f30684
Paul Karikari <paulkarikari1@gmail.com>
2019-02-14 03:02:44 +0100

    Add append method to LinkedList

--- linked_list/linked_list.go
+++ linked_list/linked_list.go
@@ -10,3 +10,18 @@ type Node struct {
 type LinkedList struct {
 	head *Node
 }
+
+func (l *LinkedList) append(data interface{}) {
+
+	if l.head == nil {
+		l.head = &Node{data: data}
+		return
+	}
+	current := l.head
+	for current.next != nil {
+		current = current.next
+	}
+
+	current.next = &Node{data: data}
+
+}
