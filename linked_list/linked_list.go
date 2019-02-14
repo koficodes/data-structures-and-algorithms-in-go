c45e08b235bb39c916570ff6f260d39037025b6b
Paul Karikari <paulkarikari1@gmail.com>
2019-02-14 02:52:12 +0100

    Add LinkedList struct

--- linked_list/linked_list.go
+++ linked_list/linked_list.go
@@ -1,7 +1,12 @@
 package linkedlist
 
-//Node types in linked list
+//Node type in linked list
 type Node struct {
 	data interface{}
 	next *Node
+}
+
+//LinkedList container for Nodes
+type LinkedList struct {
+	head *Node
 }
